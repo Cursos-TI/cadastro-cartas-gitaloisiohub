60b40d6db80089bd005ebc5b07b8627fdda6db77
gitaloisiohub <aloisiojuniorprimeiro@gmail.com>
2025-10-08 14:41:39 -0300

    Update DesafioCartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,26 +5,34 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  char populacao[50];
-  char area[50];
+    // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado;
+  char código da carta[50]
+  int populacao;
+  float área em km²;
   float pib;
-  int numero;
+  int numero de pontos turisticos;
+
+  //Área para entrada de dados
+  printf("Digite o estado: \n");
+  scanf("%s", &estado);
+
+  printf("Digite o código da carta: \n);
+  scanf("%s", codigo da carta);
 
   printf("Digite a populacao: \n");
-  scanf("%s", &populacao);
+  scanf("%d", populacao);
 
-  printf("Digite a area: \n");
-  scanf("%s", &area);
+  printf("Digite a area\n");
+  scanf("%f", area);
 
   printf("Digite o pib: \n");
-  scanf("%f", &pib);
+  scanf("%f", pib);
 
   printf("Digite o numero de pontos turisticos: \n");
-  scanf("%d", &numero);
-
-
+  scanf("%d", numero de pontos turisticos);
   
-
+  
 
 return 0;
 } 
